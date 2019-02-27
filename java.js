8b402f66d31a6fb0fe3c2ae05523b324a641874c
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-02-27 11:32:21 -0800

    css is over,working on JS

---
+++ java.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+        //variables
+        var form = document.getElementById("form");
+        var input = document.getElementsById("input");
+        var btn = document.getElementById("btn");
+        var list = document.getElementById("list");
+        var id = 1;
+        //button event listener
+        btn.addEventListener("click",addToDoItem);
+
+
+        // add todo item list
+        function addToDoItem(){
+            if(input.value === ""){
+                alert("You must enter some value!");
+            }
+            else{
+                if(list.style.borderTop === ""){
+                    list.style.bordertop = "2px solid white";
+                }
+                var text = input.value;
+            }
+        }
+
+}

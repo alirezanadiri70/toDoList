42b7b51e93ddf62e1c8f0383b60af4937c5b0840
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-02-27 22:35:22 -0800

    changes on Css,Java.js,index

--- java.js
+++ java.js
@@ -1,27 +1,29 @@
-window.onload = function(){
-        //variables
-        var form = document.getElementById("form");
-        var input = document.getElementsById("input");
-        var btn = document.getElementById("btn");
-        var list = document.getElementById("list");
-        var id = 1;
-        //button event listener
-        btn.addEventListener("click",addToDoItem);
+// window.onload = function(){
+//         //variables
+//         var form = document.getElementById("form");
+//         var input = document.getElementsById("input");
+//         var btn = document.getElementById("btn");
+//         var list = document.getElementById("list");
+//         var id = 1;
+//         //button event listener
+//         btn.addEventListener("click",addToDoItem);
 
 
-        // add todo item list
-        function addToDoItem(){
-            if(input.value === ""){
-                alert("You must enter some value!");
-            }
-            else{
-                if(list.style.borderTop === ""){
-                    list.style.bordertop = "2px solid white";
-                }
-                var text = input.value;
-                var item = `<li id="li-${id}">${text}
-                            <input id= "box-${id}" class= "checkboxes" type="checkbox"
-            }
-        }
+//         // add todo item list
+//         function addToDoItem(){
+//             if(input.value === ""){
+//                 alert("You must enter some value!");
+//             }
+//             else{
+//                 if(list.style.borderTop === ""){
+//                     list.style.bordertop = "2px solid white";
+//                 }
+//                 var text = input.value;
+//                 var item = `<li id="li-${id}">${text}
+//                             <input id= "box-${id}" class= "checkboxes" type="checkbox"
+//             }
+//         }
 
-}
+// }
+
+console.log("Hello World");
